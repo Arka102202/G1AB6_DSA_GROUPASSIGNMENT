467581536ceb8434f91870f0f73461c290e49c47
Arka102202 <111365238+Arka102202@users.noreply.github.com>
2022-11-12 08:56:43 +0530

    committing

---
+++ com/greateLearning/groupG1AB6/Node.java
@@ -0,0 +1,10 @@
+package com.greateLearning.groupG1AB6;
+
+public class Node{
+    Integer val;
+    Node left,right;
+    public Node(Integer val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
