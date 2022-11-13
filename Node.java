fa84acc88b9a3c7f88fce7ccc0872ec3e3387d76
Arka102202 <111365238+Arka102202@users.noreply.github.com>
2022-11-13 12:29:14 +0530

    final update

---
+++ Node.java
@@ -0,0 +1,10 @@
+package com.greateLearning.groupG1AB6.problem2_Data_Storage_MNC;
+
+public class Node{
+    Integer val;
+    Node left,right;
+    public Node(Integer val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
